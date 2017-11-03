799884f3dd47b94dcd4204f025c742fc1b0c365f
chenyu <chenyu@github.com>
2017-11-03 10:35:04 +0800

    textModel

---
+++ GitText/textModel.h
@@ -0,0 +1,13 @@
+//
+//  textModel.h
+//  GitText
+//
+//  Created by 刘文锋 on 2017/11/3.
+//  Copyright © 2017年 陈誉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface textModel : NSObject
+
+@end
---
+++ GitText/textModel.m
@@ -0,0 +1,13 @@
+//
+//  textModel.m
+//  GitText
+//
+//  Created by 刘文锋 on 2017/11/3.
+//  Copyright © 2017年 陈誉. All rights reserved.
+//
+
+#import "textModel.h"
+
+@implementation textModel
+
+@end
