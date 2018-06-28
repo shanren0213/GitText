c1ce5f087723281b3dcecb950d1b91688fda888e
chanyu <>
2018-06-28 13:36:42 +0800

    添加一个文件夹

---
+++ 123/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  GitText
+//
+//  Created by 刘文锋 on 2017/11/2.
+//  Copyright © 2017年 陈誉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    UIView *a = [UIView new];
+    [self.view addSubview:a];
+    
+    UITextView *textView = [UITextView new];
+    
+    //Mille
+    UIImage *imageLocal = [[UIImage alloc] initWithContentsOfFile:@"GitHub"];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
